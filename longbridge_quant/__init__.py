ab9ee7a2fe679a8c68be4d2ed6cc7fd3ae811836
Jayce <783134794@qq.com>
2025-04-08 16:49:33 +0800

    从零开始

--- longbridge_quant/__init__.py
+++
@@ -1,5 +0,0 @@
-"""
-LongPort API enhanced client for quantitative trading
-"""
-
-__version__ = '1.0.0' 
--- longbridge_quant/api_client/__init__.py
+++
@@ -1,7 +0,0 @@
-"""
-API Client module providing enhanced access to LongPort API
-"""
-
-from .client import LongPortClient
-
-__all__ = ['LongPortClient'] 
